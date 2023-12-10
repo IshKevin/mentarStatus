be41181c79840ec57b189ff565b2a808551705f1
Kevin <cartek279@gmail.com>
2023-12-10 11:19:41 +0200

    Add MyMail class for sending daily health check-up emails

---
+++ app/Mail/MyMail.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class MyMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $details;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($details)
+    {
+        //
+        $this->details = $details;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'My Mail',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.myMail',
+        );
+    }
+
+    public function build()
+    {
+        return $this->subject('Daily email for health check-up')
+            ->view('emails.myMail');
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
