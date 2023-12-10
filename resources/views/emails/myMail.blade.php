1ea1217162f82ed2b5c1fff6b20e365fe527d483
Kevin <cartek279@gmail.com>
2023-12-10 11:19:52 +0200

    Add myMail.blade.php template for email

---
+++ resources/views/emails/myMail.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Codersvibe.com</title>
+</head>
+<body>
+    <h1>{{ $details['title'] }}</h1>
+    <p>{{ $details['body'] }}</p>
+     <p>u can visit rwanda on this link <a href="https://walkerworld.ai/"></a>visit rwanda</p>
+    <p>Thank you</p>
+</body>
+</html>
