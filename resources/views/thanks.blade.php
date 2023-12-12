858be75b7725e028dc82ac381d9058aa3ec65da0
Kevin <cartek279@gmail.com>
2023-12-12 14:42:07 +0200

    Add thanks.blade.php file

---
+++ resources/views/thanks.blade.php
@@ -0,0 +1,18 @@
+<?php
+
+// FILEPATH: /c:/Users/user/Documents/software Design/mentarStatus/resources/views/thanks.php
+
+// Start writing your PHP code here
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Subscribed</title>
+</head>
+<body>
+
+<!-- Start writing your HTML code here -->
+<h1>Thank you for using our service </h1>
+</body>
+</html>
